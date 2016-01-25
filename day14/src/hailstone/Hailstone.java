a5a778133ccb2d9cc809c38d938d26fab214d450
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-01-25 20:04:01 +0000

    Alternative hailstone  version

--- day14/src/hailstone/Hailstone.java
+++ day14/src/hailstone/Hailstone.java
@@ -31,6 +31,27 @@ public class Hailstone {
 
 }
 
+class HailstoneTwo {
+    //As always converges to one, cannot go to same point twice and so no advantage from memoization
+
+    public static List<Integer> hailstone(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        return getHailstone(list, n);
+
+    }
+
+    private static List<Integer> getHailstone(List<Integer> list, int n) {
+        list.add(n);
+
+        if (n == 1) return list;
+
+        if (n % 2 == 0) return getHailstone(list, n / 2);
+
+        return getHailstone(list, 3 * n + 1);
+    }
+
+}
 
 
 
