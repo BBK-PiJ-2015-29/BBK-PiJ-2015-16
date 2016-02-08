25d38e8035d896856e697a0f5d07de52bb201279
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-02-08 20:43:01 +0000

    Examples from Feb 8th

---
+++ 2016-02-08/src/Cat.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static java.lang.System.exit;
+
+public class Cat {
+    public static void main(String... args) {
+        if (args.length != 1) {
+            System.err.println("The number of arguments should be one file name");
+            exit(-1);
+        }
+
+        File f = new File(args[0]);
+        if (!f.exists()) {
+            System.err.println("Sorry, the file with name [" + args[0] + "] does not exist!");
+            exit(-1);
+        }
+
+        cat(f);
+    }
+
+    private static String cat(File f) {
+        StringBuilder s = new StringBuilder("output: ");
+
+
+        try (BufferedReader b = new BufferedReader(new FileReader(f))) {
+            String str;
+            while ((str = b.readLine()) != null) {
+                s.append(str);
+                s.append("\n");
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return s.toString();
+    }
+
+    private void meth(Object... stuff) {  // just for show
+    }
+}
