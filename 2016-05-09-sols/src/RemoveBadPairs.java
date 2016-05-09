8305469378bc405c8bfacd77c148d960f88f31e3
Keith Mannock <keith@dcs.bbk.ac.uk>
2016-05-09 20:02:41 +0100

    Examples for 9th May 2016

--- 2016-05-09-sols/src/RemoveBadPairs.java
+++ 2016-05-09-sols/src/RemoveBadPairs.java
@@ -31,19 +31,20 @@ public class RemoveBadPairs {
   public static void removeBadPairsAgainAgain(List<Integer> list) {
     if (list.size() % 2 != 0) {
       list.remove(list.size() - 1);
+    }
 
-      int limit = list.size();
-      for (int i = 0; i < limit; i += 2) {
-        if (list.get(i) <= list.get(i + 1)) {
-          list.add(list.get(i));
-          list.add(list.get(i+1));
-        }
-      }
-      for (int i=0; i < limit; i++){
-        list.remove(0);
+    int limit = list.size();
+    for (int i = 0; i < limit; i += 2) {
+      if (list.get(i) <= list.get(i + 1)) {
+        list.add(list.get(i));
+        list.add(list.get(i + 1));
       }
     }
+    for (int i = 0; i < limit; i++) {
+      list.remove(0);
+    }
   }
+
 
   public static void main(String[] args) {
     List<Integer> lst = new ArrayList(Arrays.asList(3, 7, 9, 2, 5, 5, 8, 5, 6, 3, 4, 7, 3, 1));
